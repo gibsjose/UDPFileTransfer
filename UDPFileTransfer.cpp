3032504a9e219c6f2217f2349f0496ad74dc5ad6
Seth <hilaskis@mail.gvsu.edu>
2015-03-25 11:23:18 -0400

    UDP .h file'

---
+++ UDPFileTransfer.cpp
@@ -0,0 +1,8 @@
+#include "UDPFileTransfer.h"
+
+int main(int argc, char* argv) {
+
+
+
+    return 0;
+}
---
+++ UDPFileTransfer.h
@@ -0,0 +1,17 @@
+#ifndef UDP_FILE_TRANSFER_H
+#define UDP_FILE_TRANSFER_H
+
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <iostream>
+#include <string>
+
+#endif
