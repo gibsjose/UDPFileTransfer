dd2070eecb33182305515aa38d2841f7a422f33c
mumfordr <mumfordr@mail.gvsu.edu>
2015-03-25 11:31:03 -0400

    ...

--- Client.cpp
+++ Client.cpp
@@ -11,9 +11,16 @@ int main(int argc, char **argv){
   struct sockaddr_in serveraddr;
   serveraddr.sin_family = AF_INET;
   //Add in port
-  serveraddr.sin_port = htons();
+  serveraddr.sin_port = htons(8888);
   //Add in IPv4 address
-  serveraddr.sin_addr.s_addr = inet_addr("");
+  serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+  char buff[1000];
+
+  std::cout << "What file would you like to retrieve" << std::endl;
+  std::cin.getline(buff, 1000);
+
+  // sendto(sockfd, buff, strlen(buff) + 1, 0,
+  //       (struct sockaddr *)&serveraddr, sizeof(struct sockaddr));
 
 }
