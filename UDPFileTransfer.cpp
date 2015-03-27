0182b3d1027e7474134e81b36ac3d4f85ea66dba
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-03-27 11:45:09 -0400

    ...

--- UDPFileTransfer.cpp
+++ UDPFileTransfer.cpp
@@ -5,6 +5,7 @@ int main(int argc, char * argv[]) {
     configManager.ParseArgs(argc, argv);
     configManager.PrintArgs();
 
+    //Run either the Server or Client process
     if(configManager.GetRunType() == RunServer) {
         Server server(configManager.GetPort());
         server.Run();
